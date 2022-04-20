0d5eccc5f3d4674c849c5897df3629badd0c9134
Nikita Khomitsevych <hamsternik9@gmail.com>
2022-04-21 01:06:53 +0300

    env: SwiftUI entry point

---
+++ Pilulka Coding Challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pilulka Coding Challenge
+//
+//  Created by Nikita Khomitsevych on 21.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pilulka Coding Challenge/Pilulka_Coding_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pilulka_Coding_ChallengeApp.swift
+//  Pilulka Coding Challenge
+//
+//  Created by Nikita Khomitsevych on 21.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct Pilulka_Coding_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
